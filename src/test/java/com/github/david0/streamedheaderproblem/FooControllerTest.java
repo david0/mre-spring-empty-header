3553fe148898027d754b9657259b54c9c56ff2c0
David <david0@users.github.com>
2024-07-08 15:37:02 +0200

    test new spring version + netty

--- src/test/java/com/github/david0/streamedheaderproblem/FooControllerTest.java
+++ src/test/java/com/github/david0/streamedheaderproblem/FooControllerTest.java
@@ -14,7 +14,8 @@ class FooControllerTest {
 
 	@Test
 	void test(){
-		for(int i=0;i<10000; i++) {
+		for(int i=0;i<100000; i++) {
+			System.out.println(i);
 			webClient.post()
 					.uri("/foo")
 					.accept(MediaType.APPLICATION_NDJSON)
